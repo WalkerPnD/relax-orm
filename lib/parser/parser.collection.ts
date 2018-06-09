f6f24693af29c0b5eb893af30aad332cc9685d6b
Koji Carvalho <stylethewalker@gmail.com>
2018-06-09 19:43:56 -0300

    fix(Build): remove unused file

--- lib/parser/parser.collection.ts
+++
@@ -1,8 +0,0 @@
-import { Op } from '../enum/operator.type';
-import { parseAnd } from './and.parser';
-import { parseOr } from './or.parser';
-
-export const RootParserCollection = {
-  [Op.or]: parseOr,
-  [Op.and]: parseAnd,
-};
